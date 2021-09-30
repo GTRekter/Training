004b032d09177c94713256b72cb4fe5287324c4b
Ivan Porta <porta.ivan@outlook.com>
2021-10-01 00:28:17 +0200

    Move files

---
+++ 4_Terraform/module/local/local_file.tf
@@ -0,0 +1,4 @@
+resource "local_file" "message" {
+    filename = "/mnt/c/Users/ivanporta/Desktop/output.txt"
+    content = "Hello world"
+}
--- 4_Terraform/variables/local_file_with_internal_variables.tf
+++ 4_Terraform/variables/local_file_with_internal_variables.tf
@@ -1,3 +1,5 @@
+variable "content" {
+}
 resource "local_file" "message" {
     filename = "/mnt/c/Users/ivanporta/Desktop/output.txt"
     content = var.content
