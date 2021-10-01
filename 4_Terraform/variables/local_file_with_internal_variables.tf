7fc76c9c81195f7eecda121764a3baaa5f9c7b88
Ivan Porta <porta.ivan@outlook.com>
2021-10-01 11:58:12 +0200

    add validation, description and validation

--- 4_Terraform/variables/local_file_with_internal_variables.tf
+++ 4_Terraform/variables/local_file_with_internal_variables.tf
@@ -1,12 +1,23 @@
-variable "filename" {
+variable "partialstringfilename" {
     type = number
+    default = "file"
+}
+variable "partialnumberfilename" {
+    type = number
+    description = "File's number"
+    sensitive = true
 }
 variable "extension" {
     type = string
+    description = "File's extension. Value accepted: txt, jpg"
+    validation = validation {
+        condition = anytrue([var.extension == "txt", var.extension == "jpg"])
+        error_message = "Extension not supported! Change it to txt or jpg."
+    }
 }
 variable "content" {
 }
 resource "local_file" "message" {
-    filename = "/mnt/c/Users/ivanporta/Desktop/${var.filename}.${var.extension}"
+    filename = "/mnt/c/Users/ivanporta/Desktop/${var.partialstringfilename}-${var.partialnumberfilename}.${var.extension}"
     content = var.content
 }
